e05ffaae07c92e6b713ddfed46b6816fe10a6a9f
Anthony Parsons <flussence@gmail.com>
2014-07-20 21:12:17 +0100

    Fix serial invocation; all tests pass
    (I am by definition not smart enough to debug this code)

--- lib/Test/Corpus.pm
+++ lib/Test/Corpus.pm
@@ -2,14 +2,16 @@ module Test::Corpus:auth<github:flussence>:ver<2.0.0-rc>;
 
 use Test;
 
-#= Convenience sub for testing filter functions of arity 1
+#| Convenience sub for testing filter functions of arity 1
 sub simple-test(&func) is export {
+    # ^^ This wants to be "&func:(Str --> Str)"
+
     return sub (IO::Handle $in, IO::Handle $out, Str $testcase) {
         is &func($in.slurp), $out.slurp, $testcase;
     }
 }
 
-#= Runs tests on a callback. The callback gets passed input/output filehandles,
+#| Runs tests on a callback. The callback gets passed input/output filehandles,
 #  and the basename of the test file being run. Tests are run in no particular
 #  order.
 sub run-tests(
@@ -34,7 +36,7 @@ sub run-tests(
     # (tested on Rakudo 2014.07-6-g8668171aa34a)
     given $parallel {
         when 'serial' {
-            @files.map(&test-closure);
+            &test-closure($_).() for @files;
         }
         when 'hyper' {
             @files».&test-closure».();
