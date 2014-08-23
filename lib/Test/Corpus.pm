0a0526faca687d92c77ff9aa36a0dfe1a376ee4c
Anthony Parsons <flussence@gmail.com>
2014-08-23 17:47:16 +0100

    Re-add the »-based codepath, bump version to 2.0.0
    Current monthly release of rakudo-moar seems to be able to handle it again.
    
    Also properly document exactly how the extreme-optimisation version doesn't work
    so that I can come back to it later.

--- lib/Test/Corpus.pm
+++ lib/Test/Corpus.pm
@@ -1,4 +1,4 @@
-module Test::Corpus:auth<github:flussence>:ver<2.0.0-rc.2>;
+module Test::Corpus:auth<github:flussence>:ver<2.0.0>;
 
 use Test;
 
@@ -31,13 +31,30 @@ sub run-tests(
         );
     }
 
-    # This will parallelise in the future, don't make assumptions about order.
-    &test-closure($_).() for @files;
-
-    # Two possible alternatives to the above (note that these both currently
-    # crash in MoarVM, so aren't used):
-    #   @files».&test-closure».();
-    #   await @files».&test-closure».&start;
+    @files».&test-closure».();
 }
+
+=begin pod
+This module would be a good candidate to use threading and I've been trying to
+find a way to pull this off for a while:
+
+    await @files».&test-closure».&start;
+
+Ideally everything would run nice and fast and your CPU would burst into flames.
+
+We can't do that (yet) for various reasons:
+
+=item
+It just straight up crashes moarvm, before completing the module's own tests
+(and worse, some of the ones that do run randomly fail!).
+
+=item
+Threads + say/print just don't mix. This code also doesn't seem particularly
+amenable to my attempts to override $*OUT with an object that does that, and
+just spits everything straight to /dev/stdout regardless. The amount of
+difficulty here makes me think I'm trying to solve the thread-safety problem at
+the wrong level.
+
+=end pod
 
 # vim: set tw=80 :
